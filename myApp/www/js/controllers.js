92dee315ee54e7849f1c0383740e8a22ee086c42
Eugénie Birolleau <eugenie.birolleau@gmail.com>
2016-05-24 16:15:13 +0200

    img en bonne qualite

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -44,11 +44,9 @@ angular.module('starter.controllers', [])
 .controller('PlaylistsCtrl', function($scope) {
   var vm = this;
   vm.items = [
-    {title: 'Item 1', desc: '../img/food.jpg'},
-    {title: 'Item 2', desc: '../img/food2.jpg'},
-    {title: 'Item 2', desc: '../img/food3.jpg'},
-    {title: 'Item 2', desc: '../img/food4.jpg'},
-    {title: 'Item 3', desc: '../img/food5.jpg'}    
+    {title: 'Item 1', desc: '../img/slide.jpg'},
+    {title: 'Item 2', desc: '../img/slide2.jpg'},
+    {title: 'Item 2', desc: '../img/slide3.jpg'}    
   ];
   
   vm.onSlideChanged = function(slideIndex) {
